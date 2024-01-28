b7134e29e0a10c7f314873562923454ba745e057
Leevi Ilmari Puntanen <leevi.puntanen2@gmail.com>
2024-01-28 22:27:09 +0200

    fixed habbitcard.tsx

--- habbit-tracker/app/components/habbitCard.tsx
+++ habbit-tracker/app/components/habbitCard.tsx
@@ -9,7 +9,7 @@ interface HabbitCardProps {
   setDays: React.Dispatch<React.SetStateAction<Day[]>>;
 }
 
-const HabbitCard: React.FC<HabbitCardProps> = ({day, setDays}) => {
+const HabbitCard = ({day, setDays}: HabbitCardProps) => {
   const [habbitName, setHabbitName] = useState<string>('');
   const [edit, setEdit] = useState<boolean>(false);
   const [editId, setEditId] = useState<string>();
@@ -29,90 +29,56 @@ const HabbitCard: React.FC<HabbitCardProps> = ({day, setDays}) => {
     setEdit(false);
   };
 
-  const handleEditHabbit = () => {
-    const updateHabitStatus = (
-      dayId: string,
-      habitId: string,
-      newStatus: boolean
-    ) => {
-      setDays((prevDays) =>
-        prevDays.map((day) =>
-          day.id.toString() === dayId.toString()
-            ? {
-                ...day,
-                habbits: day.habbits.map((habit) =>
-                  habit.id.toString() === habitId.toString()
-                    ? {...habit, status: newStatus}
-                    : habit
-                ),
-              }
-            : day
-        )
-      );
-    };
-
-    const checkIfToday = () => {};
-
-    return (
-      <div>
-        <div
-          key={day.id}
-          className=' bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'
-        >
-          <p>{day.date}</p>
-          {edit && day.id?.toString() === editId?.toString() ? (
-            <div>
-              <Input
-                value={habbitName}
-                onChange={(e) => setHabbitName(e.target.value)}
-              />
-              <Button onClick={() => handleEditHabbit()}>Save</Button>
-              <Button onClick={handleCancel}>Cancel</Button>
-            </div>
-          ) : (
-            <div>
-              {day.habbits.map((habit) => (
-                <div key={habit.name}>
-                  <Checkbox
-                    disabled={day.date !== today ? !editPast : false}
-                    checked={habit.status}
-                    onChange={(e) =>
-                      updateHabitStatus(
-                        day.id.toString(),
-                        habit.id,
-                        e.target.checked
-                      )
-                    }
-                  >
-                    {habit.name}
-                  </Checkbox>
-                </div>
-              ))}
-            </div>
-          )}
-          {!edit && day.date !== today ? (
-            editPast ? (
-              <Button onClick={() => setEditPast(false)}>Save</Button>
-            ) : (
-              <Button onClick={() => setEditPast(true)}>Edit</Button>
-            )
-          ) : null}
-          {day.date === today && !edit ? (
-            <Button onClick={() => handleEdit(day.id)} type='dashed'>
-              Add new Habbit
-            </Button>
-          ) : null}
+  return (
+    <div>
+      <div
+        key={day.id}
+        className=' bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'
+      >
+        <p>{day.date}</p>
+        {edit && day.id?.toString() === editId?.toString() ? (
           <div>
-            {`Percentage of completed habbits: ${(
-              (day.habbits.filter((h) => h.status).length /
-                day.habbits.length) *
-              100
-            ).toFixed(1)} `}
+            <Input
+              value={habbitName}
+              onChange={(e) => setHabbitName(e.target.value)}
+            />
+            <Button onClick={handleCancel}>Cancel</Button>
           </div>
+        ) : (
+          <div>
+            {day.habbits.map((habit) => (
+              <div key={habit.name}>
+                <Checkbox
+                  disabled={day.date !== today ? !editPast : false}
+                  checked={habit.status}
+                >
+                  {habit.name}
+                </Checkbox>
+              </div>
+            ))}
+          </div>
+        )}
+        {!edit && day.date !== today ? (
+          editPast ? (
+            <Button onClick={() => setEditPast(false)}>Save</Button>
+          ) : (
+            <Button onClick={() => setEditPast(true)}>Edit</Button>
+          )
+        ) : null}
+        {day.date === today && !edit ? (
+          <Button onClick={() => handleEdit(day.id)} type='dashed'>
+            Add new Habbit
+          </Button>
+        ) : null}
+        <div>
+          {`Percentage of completed habbits: ${(
+            (day.habbits.filter((h) => h.status).length / day.habbits.length) *
+            100
+          ).toFixed(1)} `}
         </div>
       </div>
-    );
-  };
+    </div>
+  );
 };
 
 export default HabbitCard;
